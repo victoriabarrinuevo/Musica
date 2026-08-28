ac179e133b5870323759a1c51e1dcbc5cb20ee74
Victória <victoriabarrinuevo02@gmail.com>
2026-08-28 18:47:00 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Loser - Tame Impala");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("I had to tell ya, it's now or never");
+Console.WriteLine("So much for closure, I lost composure");
+Console.WriteLine("I get the message, I learned my lesson");
+Console.WriteLine("Tried to correct it, I think I wrecked it");
+Console.WriteLine("Man, it's a crisis, I'm never like this");
+Console.WriteLine("That's how my life is, you couldn't write this");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("I'm a loser, babe");
+Console.WriteLine("Do you wanna tear my heart out?");
+Console.WriteLine("I'm a tragedy");
+Console.WriteLine("Tryna figure this whole mess out");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("I'm out of favor, my worst behavior");
+Console.WriteLine("I get the message, I learned my lesson");
+Console.WriteLine("Desperate times call for desperate measures");
+Console.WriteLine("I fell into ya, I fell into ya");
+Console.WriteLine("Tried to correct it, well, shit, I wrecked it ");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("I'm a loser, babe");
+Console.WriteLine("Do you wanna tear my heart out?");
+Console.WriteLine("I'm a tragedy");
+Console.WriteLine("Tryna figure my whole life out");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("I leave alone and");
+Console.WriteLine("Dark streets, I roam in");
+Console.WriteLine("Night air, I breathe in");
+Console.WriteLine("The stars, I believe in");
+Console.WriteLine();
+
+
+Console.WriteLine("I don't know why I didn't fight it");
+Console.WriteLine("I probably tried and magnified it");
+Console.WriteLine("I cannot lie, I feel defeated");
+Console.WriteLine("Take it as a sign, you're badly needed");
+Console.WriteLine();
+Console.WriteLine("You're badly needed");
+Console.WriteLine("Badly wanted");
+Console.WriteLine("Ah, f*ck");
